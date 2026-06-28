cc72b67add0fd62af7fe07f07bcb31dcfb0d2350
aaandou <gaaandou@gmail.com>
2026-06-28 19:52:52 +0900

    Set Android app icon and label to Shot Point Viewer
    Closes #5

--- lib/app.dart
+++ lib/app.dart
@@ -38,7 +38,7 @@ class GolfSwingAnalyzerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'GolfSwingAnalyzer',
+      title: 'Shot Point Viewer',
       theme: appTheme,
       routerConfig: _router,
       debugShowCheckedModeBanner: false,
